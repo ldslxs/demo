8e96539091afe01534a8fbc79415133a3c3618ab
李小帅 <1761874988@qq.com>
2023-07-25 09:59:45 +0000

    update src/main/java/com/bigdata/utils/HBaseUtils.java.
    Signed-off-by: 李小帅 <1761874988@qq.com>

--- src/main/java/com/bigdata/utils/HBaseUtils.java
+++ src/main/java/com/bigdata/utils/HBaseUtils.java
@@ -17,10 +17,7 @@ public class HBaseUtils {
 //李晓帅
 //李晓帅
 //李晓帅
-//李晓帅
-//李晓帅
-//李晓帅
-//李晓帅
+//李晓帅12312312312
     public static Connection connection = null;
     // static 在进入jvm虚拟机的时候就会执行 static
     static{
