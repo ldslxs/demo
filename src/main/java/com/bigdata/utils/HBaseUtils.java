30f635f10b6cd6828419a5a7cf54e5afe1a6ac21
李小帅 <1761874988@qq.com>
2023-07-25 17:57:06 +0800

    update src/main/java/com/bigdata/utils/HBaseUtils.java.
    Signed-off-by: 李小帅 <1761874988@qq.com>

--- src/main/java/com/bigdata/utils/HBaseUtils.java
+++ src/main/java/com/bigdata/utils/HBaseUtils.java
@@ -16,6 +16,8 @@ public class HBaseUtils {
 //李晓帅
 //李晓帅
 //李晓帅
+//李晓帅
+//李晓帅
     public static Connection connection = null;
     // static 在进入jvm虚拟机的时候就会执行 static
     static{
@@ -28,7 +30,6 @@ public class HBaseUtils {
             e.printStackTrace();
         }
     }
-    //lasdalsdpals;dl;askdlaskdl;asdk
 
     // 获取admin对象方法
     public static Admin getAdmin(){
